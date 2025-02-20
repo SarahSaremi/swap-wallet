89a383cb7c98a48b2493b6f0a8c0e8e9234f66ae
Sarah Saremi <sarasaaremi@gmail.com>
2025-02-20 16:04:08 +0330

    change coin str

--- swapWallet/swap/models/coin.py
+++ swapWallet/swap/models/coin.py
@@ -6,4 +6,4 @@ class Coin(models.Model):
     symbol = models.CharField(max_length=10, unique=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} ({self.symbol})"
